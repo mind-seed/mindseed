cd5358d4c50f62226543091ee069769dd414cdc8
kimsaram32 <kimsaram32@proton.me>
2026-03-11 21:54:59 +0900

    chore(backend): TypeORM 설정

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigType } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { databaseConfig } from "./config/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [databaseConfig.KEY],
+      useFactory: (db: ConfigType<typeof databaseConfig>) => ({
+        type: "postgres",
+        host: db.host,
+        port: db.port,
+        username: db.username,
+        password: db.password,
+        database: db.database,
+        entities: [],
+        synchronize: process.env.NODE_ENV !== "production",
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
