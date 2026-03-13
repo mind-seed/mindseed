d30cdf63ccf2dec22369918e83c2aaef205f0caa
kimsaram32 <kimsaram32@proton.me>
2026-03-13 21:11:43 +0900

    feat(api-types): 회원가입 API type schema 및 build step 추가
    - 일반적인 request/response type schema 정의
    - /auth/send-mail, /auth/verify-mail, /auth/complete-signup에 대한
      request/response type schema 정의
    - tsup 기반 build step 추가

--- packages/api-types/src/index.ts
+++ packages/api-types/src/index.ts
@@ -1,11 +1,2 @@
-import * as z from "zod";
-
-export const TestRequestDtoSchema = z.object({
-  name: z.string().min(3).max(10),
-});
-
-export type TestRequestDto = z.infer<typeof TestRequestDtoSchema>;
-
-export type TestResponseDto = {
-  greeting: string;
-};
+export * from "./auth";
+export * from "./shared";
