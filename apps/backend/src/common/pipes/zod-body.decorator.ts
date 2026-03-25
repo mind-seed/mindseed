39f0e0145d5919b3eb2db2f8ca680fe0440afe6d
kimsaram32 <kimsaram32@proton.me>
2026-03-25 22:58:50 +0900

    feat(backend): Zod validation decorator에 query, param 타입 추가

--- apps/backend/src/common/pipes/zod-body.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { Body } from "@nestjs/common";
-import { ZodType } from "zod";
-import { ZodDecoderPipe } from "./zod-decoder.pipe";
-
-export const ZodBody = (schema: ZodType) => Body(new ZodDecoderPipe(schema));
