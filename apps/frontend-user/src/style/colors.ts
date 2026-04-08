d83bd2bc9779288ee89a8c186f57d7ce237f49a3
jsl0118 <jsl_0118@dsm.hs.kr>
2026-04-08 10:27:04 +0900

    feat(frontend-user): style-component 추가 및 color, text 상수 설정

---
+++ apps/frontend-user/src/style/colors.ts
@@ -0,0 +1,29 @@
+export const COLORS = {
+  gray: {
+    gray50: "#FFFFFF",
+    gray100: "#F8F8F8",
+    gray200: "#E2E2E2",
+    gray300: "#C9C9C9",
+    gray400: "#ADADAD",
+    gray500: "#919191",
+    gray600: "#747474",
+  },
+  main: {
+    disabled: "#D4DFC4",
+    back: "#C0EF7E",
+    ligther: "#C0EF7E",
+    main: "#97BC62",
+    darker: "#586F37",
+  },
+  state: {
+    info: "#2F80ED",
+    success: "#27AE60",
+    warning: "#E2B93B",
+    error: "#EB5757",
+  },
+  text: {
+    black: "#000000",
+    2: "#494736",
+    1: "#69664D",
+  },
+};
---
+++ apps/frontend-user/src/style/typography.ts
@@ -0,0 +1,58 @@
+export const TEXT_STYLE = {
+  label: {
+    md: `
+      font-weight: 400;
+      font-size: 16rem;
+      line-height: 120%;
+    `,
+  },
+  body: {
+    ti: `
+      font-weight: 400;
+      font-size: 14rem;
+      line-height: 120%;
+    `,
+    sm: `
+      font-weight: 400;
+      font-size: 16rem;
+      line-height: 120%;
+    `,
+    md: `
+      font-weight: 400;
+      font-size: 18rem;
+      line-height: 120%;
+    `,
+    md2: `
+      font-weight: 600;
+      font-size: 18rem;
+      line-height: 120%;
+    `,
+    lg: `
+      font-weight: 400;
+      font-size: 20rem;
+      line-height: 120%;
+    `,
+  },
+  title: {
+    ti: `
+      font-weight: 600;
+      font-size: 16rem;
+      line-height: 120%;
+    `,
+    sm: `
+      font-weight: 600;
+      font-size: 24rem;
+      line-height: 120%;
+    `,
+    md: `
+      font-weight: 600;
+      font-size: 36rem;
+      line-height: 120%;
+    `,
+    lg: `
+      font-weight: 600;
+      font-size: 48rem;
+      line-height: 120%;
+    `,
+  },
+};
