227ddb15a6f3698af93def1b63c9db1563b39895
kimsaram32 <kimsaram32@proton.me>
2026-04-14 12:30:20 +0900

    refactor(backend): TypeORM configuration을 별개 모듈로 분리

---
+++ apps/backend/src/database/database.module.ts
@@ -0,0 +1,46 @@
+import { Module } from "@nestjs/common";
+import { ConfigType } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { databaseConfig } from "src/config";
+import { User } from "src/user/entities/user.entity";
+import { UserProfile } from "src/user/entities/user-profile.entity";
+import { RefreshToken } from "src/auth/refresh-token/refresh-token.entity";
+import { Attachment } from "src/attachment/entities/attachment.entity";
+import { Post } from "src/post/entities/post.entity";
+import { PostComment } from "src/comment/entities/post-comment.entity";
+import { PostLike } from "src/post/entities/post-like.entity";
+import { Resource } from "src/resource/entities/resource.entity";
+import { Mission } from "src/mission/entities/mission.entity";
+import { MissionAssignment } from "src/mission/entities/mission-assignment.entity";
+import { DiagnosisEntry } from "src/diagnosis/entities/diagnosis-entry.entity";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [databaseConfig.KEY],
+      useFactory: (db: ConfigType<typeof databaseConfig>) => ({
+        type: "postgres",
+        host: db.host,
+        port: db.port,
+        username: db.username,
+        password: db.password,
+        database: db.database,
+        entities: [
+          Attachment,
+          User,
+          UserProfile,
+          RefreshToken,
+          Post,
+          PostComment,
+          PostLike,
+          Resource,
+          Mission,
+          MissionAssignment,
+          DiagnosisEntry,
+        ],
+        synchronize: process.env.NODE_ENV !== "production",
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
