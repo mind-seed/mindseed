c1da2e316f681db46d1afbe864e41b2c05f09b98
kimsaram32 <kimsaram32@proton.me>
2026-04-14 19:43:52 +0900

    refactor(api-types): Date -> Temporal migration

--- packages/api-types/src/common/codecs.ts
+++ packages/api-types/src/common/codecs.ts
@@ -1,9 +1,14 @@
+import { Temporal } from "@js-temporal/polyfill";
 import z from "zod";
 
-export const dateSerializerCodec = z.codec(z.iso.datetime(), z.date(), {
-  decode: (str) => new Date(str),
-  encode: (date) => date.toISOString(),
-});
+export const dateSerializerCodec = z.codec(
+  z.iso.datetime(),
+  z.instanceof(Temporal.Instant),
+  {
+    decode: (str) => Temporal.Instant.from(str),
+    encode: (instant) => instant.toString(),
+  },
+);
 
 export const numberSerializerCodec = z.codec(
   z.string().regex(z.regexes.number),
