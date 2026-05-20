0926f23065ff9a4b7f71bb0781b00579b75fe4b5
kimsaram32 <kimsaram32@proton.me>
2026-05-20 19:05:36 +0900

    feat(backend): UserService에 updateProfile 작성

--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserService } from "./user.service";
 import { User } from "./entities/user.entity";
+import { UserProfile } from "./entities/user-profile.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserProfile])],
   providers: [UserService],
   exports: [UserService],
 })
