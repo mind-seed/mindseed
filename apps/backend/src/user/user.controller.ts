9d20ff4fb575ebc91a301380253947255abf4f96
kimsaram32 <kimsaram32@proton.me>
2026-05-27 20:04:33 +0900

    feat(backend): 회원탈퇴 및 프로필 수정 API controller 작성

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,17 +1,33 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
 import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Patch,
+} from "@nestjs/common";
+import {
+  DeleteCurrentUserResponseDtoSchema,
   GetCurrentUserResponseDtoSchema,
+  UpdateCurrentUserRequestDtoSchema,
+  UpdateCurrentUserResponseDtoSchema,
+  type DeleteCurrentUserSuccessResponseDto,
+  type UpdateCurrentUserRequestDto,
   type GetCurrentUserSuccessResponseDto,
+  type UpdateCurrentUserSuccessResponseDto,
 } from "@mindseed/api-types";
 import {
   Authenticated,
   CurrentUser,
+  UserOnly,
 } from "src/auth/decorators/auth.decorators";
 import { ZodEncodeResponse } from "src/common/interceptors/zod-encode-response.decorator";
 import { User, UserRole } from "./entities/user.entity";
 import { UserRoleSchema } from "@mindseed/api-types";
 import { z } from "zod";
 import { createBiMap } from "src/common/helpers/mapper";
+import { UserService } from "./user.service";
+import { ZodBody } from "src/common/pipes/zod-validation.decorator";
 
 const roleMap = createBiMap<UserRole, z.output<typeof UserRoleSchema>>([
   [UserRole.USER, "user"],
@@ -20,6 +36,8 @@ const roleMap = createBiMap<UserRole, z.output<typeof UserRoleSchema>>([
 
 @Controller("/users")
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get("/current")
   @HttpCode(HttpStatus.OK)
   @Authenticated()
@@ -40,6 +58,41 @@ export class UserController {
           characterIndex: user.profile.characterIndex,
         },
       },
+    };
+  }
+
+  @Patch("/current/profile")
+  @HttpCode(HttpStatus.OK)
+  @UserOnly()
+  @ZodEncodeResponse(UpdateCurrentUserResponseDtoSchema)
+  async updateCurrent(
+    @CurrentUser() user: User,
+    @ZodBody(UpdateCurrentUserRequestDtoSchema)
+    body: UpdateCurrentUserRequestDto,
+  ): Promise<UpdateCurrentUserSuccessResponseDto> {
+    await this.userService.updateProfile(user.id, {
+      nickname: body.nickname,
+      characterIndex: body.characterIndex,
+    });
+
+    return {
+      success: true,
+      data: null,
+    };
+  }
+
+  @Delete("/current")
+  @HttpCode(HttpStatus.OK)
+  @UserOnly()
+  @ZodEncodeResponse(DeleteCurrentUserResponseDtoSchema)
+  async deleteCurrent(
+    @CurrentUser() user: User,
+  ): Promise<DeleteCurrentUserSuccessResponseDto> {
+    await this.userService.softDelete(user.id);
+
+    return {
+      success: true,
+      data: null,
     };
   }
 }
